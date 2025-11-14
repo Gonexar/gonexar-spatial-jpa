c3868f42686f56e1b9b27d3e42219d2db066927c
SHE21 <santiagolenil@gmail.com>
2025-11-14 12:55:46 -0300

    implemented GonexarPostgisDialect class

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,45 @@
 plugins {
-    id("java")
+    `java-library`
+    kotlin("jvm") version "1.9.22"
+    kotlin("plugin.jpa") version "1.9.22"
 }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
+group = "com.gonexar"
+version = "1.0.0"
+
+java {
+    withSourcesJar()
+    withJavadocJar()
+}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-}
 
-tasks.test {
-    useJUnitPlatform()
+    // Kotlin
+    implementation(kotlin("stdlib"))
+
+    // JTS Geometry
+    api("org.locationtech.jts:jts-core:1.19.0")
+
+    // PostGIS JDBC
+    api("net.postgis:postgis-jdbc:2.6.0")
+    api("org.postgresql:postgresql:42.7.7")
+
+    // Hibernate Core
+    api("org.hibernate.orm:hibernate-core:6.4.1.Final")
+    api("org.hibernate.orm:hibernate-spatial:6.4.1.Final")
+
+
+    // Jackson
+    api("com.fasterxml.jackson.core:jackson-databind:2.15.2")
+
+    // JPA API
+    api("jakarta.persistence:jakarta.persistence-api:3.1.0")
+
+    // Lombok (opcional)
+    compileOnly("org.projectlombok:lombok:1.18.30")
+    annotationProcessor("org.projectlombok:lombok:1.18.30")
 }
---
+++ src/main/java/org/gonexar/dialect/GonexarPostgisDialect.kt
@@ -0,0 +1,41 @@
+package org.gonexar.dialect
+
+import org.hibernate.boot.model.FunctionContributions
+import org.hibernate.dialect.PostgreSQLDialect
+import org.hibernate.dialect.function.StandardSQLFunction
+import org.hibernate.query.sqm.function.SqmFunctionRegistry
+import org.hibernate.type.StandardBasicTypes
+
+class GonexarPostgisDialect : PostgreSQLDialect() {
+
+
+    override fun initializeFunctionRegistry(functionContributions: FunctionContributions) {
+        super.initializeFunctionRegistry(functionContributions)
+        val registry: SqmFunctionRegistry = functionContributions.functionRegistry
+
+        registry.register(
+            "ST_DWithin",
+            StandardSQLFunction("ST_DWithin", StandardBasicTypes.BOOLEAN)
+        )
+
+        registry.register(
+            "ST_Distance",
+            StandardSQLFunction("ST_Distance", StandardBasicTypes.DOUBLE)
+        )
+
+        registry.register(
+            "ST_Contains",
+            StandardSQLFunction("ST_Contains", StandardBasicTypes.BOOLEAN)
+        )
+
+        registry.register(
+            "ST_Intersects",
+            StandardSQLFunction("ST_Intersects", StandardBasicTypes.BOOLEAN)
+        )
+
+        registry.register(
+            "ST_AsText",
+            StandardSQLFunction("ST_AsText", StandardBasicTypes.STRING)
+        )
+    }
+}
