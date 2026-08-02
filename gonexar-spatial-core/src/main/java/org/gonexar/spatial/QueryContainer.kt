95c4c3d62f3c390545d8a8323da50ef9e59dab96
SHE21 <santiagolenil@gmail.com>
2026-08-02 19:58:45 -0300

    feat: add non-reified query(ctx, resultClass, block) overload
    Allows the DSL to be called from contexts where R can't be reified
    (e.g. interface implementations). The reified query() now delegates
    to it instead of duplicating the CriteriaQuery-building logic.

--- gonexar-spatial-core/src/main/java/org/gonexar/spatial/QueryContainer.kt
+++ gonexar-spatial-core/src/main/java/org/gonexar/spatial/QueryContainer.kt
@@ -3,30 +3,45 @@ package org.gonexar.spatial
 inline fun <E : Any, reified R : Any> query(
     ctx: SpatialContext<E>,
     noinline block: SpatialDslContainer<E, R>.() -> Unit
+): List<R> = query(ctx, R::class.java, block)
+
+fun <E : Any, R : Any> query(
+    ctx: SpatialContext<E>,
+    resultClass: Class<R>,
+    block: SpatialDslContainer<E, R>.() -> Unit
 ): List<R> {
 
     val entityManager = ctx.entityManager
     val cb = entityManager.criteriaBuilder
 
-    val query = cb.createQuery(R::class.java)
-    val root = query.from(ctx.entityClass)  // root = entidade
+    val query = cb.createQuery(resultClass)
+    val root = query.from(ctx.entityClass)
 
-    val criteriaCtx = CriteriaDslContext<E, R>(cb, root)
-    val dsl = SpatialDslContainer(criteriaCtx)
+    val criteriaCtx =
+        CriteriaDslContext<E, R>(
+            cb,
+            root
+        )
 
-    // Executa blocos DSL (projeção, where, expressões)
+    val dsl =
+        SpatialDslContainer(criteriaCtx)
+
     dsl.block()
 
-    // caso o DSL tenha definido um projection customizado (DTO)
-    val projection = criteriaCtx.projectionOrRoot()
-    query.select(projection)
+    query.select(
+        criteriaCtx.projectionOrRoot()
+    )
 
-    val predicates = criteriaCtx.predicates()
+    val predicates =
+        criteriaCtx.predicates()
+
     if (predicates.isNotEmpty()) {
         query.where(*predicates)
     }
 
-    return entityManager.createQuery(query).resultList
+    return entityManager
+        .createQuery(query)
+        .resultList
 }
 
 
